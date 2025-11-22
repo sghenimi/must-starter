ee0392446677b08985a175c61e0c346b441a92be
Soufiane Ghenimi <soufiane.ghenimi@gmail.com>
2025-11-22 01:38:17 +0100

    connect to postgresql

--- app_fastapi_postgresql/main.py
+++ app_fastapi_postgresql/main.py
@@ -1,13 +1,83 @@
-from fastapi import FastAPI
-
-from app_fastapi_postgresql import models
-from app_fastapi_postgresql.database import engine
-
-app = FastAPI()
+from pathlib import Path
+import psycopg2
 
 
-@app.get("/")
-async def root():
-    return {"message": "welcome"}
+def main():
+    _path = Path.home() / "Documents" / "script.py"
+    print(_path)
+    print("#" * 100)
 
-models.Base.metadata.create_all(bind=engine)
+conn = psycopg2.connect(
+    database="mydb_01",
+    user="Soufiane",
+    host="localhost",
+    password="postgres",
+    port=5432,
+)
+
+
+def operate_db():
+
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+    # Execute a command: create datacamp_courses table
+    cur.execute("""CREATE TABLE datacamp_courses(
+                    course_id SERIAL PRIMARY KEY,
+                    course_name VARCHAR (50) UNIQUE NOT NULL,
+                    course_instructor VARCHAR (100) NOT NULL,
+                    topic VARCHAR (20) NOT NULL);
+                    """)
+    # Make the changes to the database persistent
+    conn.commit()
+    # Close cursor and communication with the database
+    cur.close()
+    conn.close()
+
+def operate_insert():
+    cur = conn.cursor()
+
+    cur.execute(
+        "INSERT INTO datacamp_courses(course_name, course_instructor, topic) VALUES('Introduction to SQL','Izzy Weber','Julia')");
+
+    cur.execute(
+        "INSERT INTO datacamp_courses(course_name, course_instructor, topic) VALUES('Analyzing Survey Data in Python','EbunOluwa Andrew','Python')");
+
+    cur.execute(
+        "INSERT INTO datacamp_courses(course_name, course_instructor, topic) VALUES('Introduction to ChatGPT','James Chapman','Theory')");
+
+    cur.execute(
+        "INSERT INTO datacamp_courses(course_name, course_instructor, topic) VALUES('Introduction to Statistics in R','Maggie Matsui','R')");
+
+    cur.execute(
+        "INSERT INTO datacamp_courses(course_name, course_instructor, topic) VALUES('Hypothesis Testing in Python','James Chapman','Python')");
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def operate_get():
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM datacamp_courses;')
+    rows = cur.fetchall()
+    conn.commit()
+    conn.close()
+    for row in rows:
+        print(row)
+
+
+def operate_orm():
+    import sqlalchemy as db
+
+    engine = db.create_engine("postgresql://postgres@localhost:5432/mydb_01")
+    conn = engine.connect()
+    output = conn.execute(db.text("SELECT * FROM datacamp_courses"))
+    print(output.fetchall())
+    conn.close()
+
+
+if __name__ == "__main__":
+    # operate_db()
+    # operate_insert()
+    # operate_get()
+    operate_orm()
